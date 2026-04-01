87cf61d3dc8b4ad437dcdc39be0c45ac3c9abccf
Mohammed Alfarra <132915634+Moe-alfarra@users.noreply.github.com>
2026-04-01 18:29:04 -0400

    Update MemoryAllocationController.java

--- memory-allocation-simulator-backend/src/main/java/com/moeAlfarra/memory_allocation_simulator/MemoryAllocationController.java
+++ memory-allocation-simulator-backend/src/main/java/com/moeAlfarra/memory_allocation_simulator/MemoryAllocationController.java
@@ -4,9 +4,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/memory-allocate")
-@CrossOrigin(origins = {
-        "http://localhost:5173",
-        "https://memory-allocation-simulator-rose.vercel.app/"})  // Change if running frontend locally/different host
+@CrossOrigin(origins = "https://memory-allocation-simulator-rose.vercel.app")  // Change if running frontend locally/different host
 public class MemoryAllocationController {
     private final MemoryAllocationService memoryAllocationService;
 
